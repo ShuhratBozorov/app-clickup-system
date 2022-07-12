b15a84b9dd898a144a709a7c82fd454b1e792dd3
ShuhratBozorov <bozorovshukhrat24@gmail.com>
2022-07-13 00:11:33 +0500

    Completed

---
+++ src/main/java/uz/shuhratbozorov/appclickupsystem/AppClickupSystemApplication.java
@@ -0,0 +1,13 @@
+package uz.shuhratbozorov.appclickupsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppClickupSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppClickupSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/uz/shuhratbozorov/appclickupsystem/AppClickupSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.shuhratbozorov.appclickupsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppClickupSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
